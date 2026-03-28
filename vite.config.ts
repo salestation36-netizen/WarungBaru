2921d88ff3ac24b884dd217c946d38ad7905aa67
Salestation.id <salestation36@gmail.com>
2026-03-28 14:23:51 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // PENTING: Agar aset bisa dibaca di file system Android (file://)
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+  }
+});
   },
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
